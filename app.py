3ae880c9a178f8ef468450183155ecb00cea0e63
Albert Viñés <albertvines@MacBook-MacBook-Pro-de-Albert.local>
2016-10-08 16:04:45 +0200

    Behaviour method ready for testing

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
 from flask import Flask, jsonify
-import json
-import request
+from flask import json
+from flask import request
+
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
     return "Hello World!"
 
-@app.route("/create_event", methods=["POST"])# POST
-def create_event():
-    #data = request["data"]
-    #data = json.loads(request.data)
-    #if data.get('password') != "gentleman123":
-        #return "Wrong password!"
+@app.route('/authenticate', methods=['PUT'])
+def authenticate():
+    info = json.loads(request.data)
+    try:
+        pwd = info['password']
+        # get an admin session
+        return jsonify({'pwd': pwd}), 200
+    except Exception as e:
+        return str(e)
 
-    print(request.data)
-    return "thanks for your visit"
-
-@app.route("/<string:pos_x>/<string:pos_y>", methods=["GET"])# GET INFO
+@app.route('/get_instructions/<string:pos_x>/<string:pos_y>', methods=["GET"])
 def get_instructions(pos_x,pos_y):
-    return "Geting instructions for the event " + " at " + pos_x + " " + " " + pos_y
+    return jsonify({'behaviour': [{
+                        'color': "#FFF000",
+                        'flash': "yes",
+                        'vibrate': "yes",
+                        'duration': "1000",
+                        'shake': "yes"
+                        },{
+                        'color': "#F00FF",
+                        'flash': "no",
+                        'vibrate': "no",
+                        'duration': "1000",
+                        'shake': "no"
+                        }
+                    ]})
 
 @app.route("/add_figure/<string:event_name>", methods=["POST"])
 def add_figure(event_name):
